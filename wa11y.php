352f90beebd501d1b56ceea38eedce300963441c
Rachel Carden <bamadesigner@gmail.com>
2015-06-22 09:22:04 -0500

    Comment out axe and add user capability to tota11y

--- wa11y.php
+++ wa11y.php
@@ -23,12 +23,13 @@ add_action( 'wp_enqueue_scripts', 'wally_enqueue_scripts' );
 function wally_enqueue_scripts() {
 
 	// Register axe - goes in header
-	wp_register_script( 'axe', plugins_url( '/includes/axe/axe.min.js' , __FILE__ ) );
+	//wp_register_script( 'axe', plugins_url( '/includes/axe/axe.min.js' , __FILE__ ) );
 
 	// Initiate axe - goes in header
-	wp_enqueue_script( 'initiate-axe', plugins_url( '/includes/axe/initiate-axe.js' , __FILE__ ), array( 'axe' ) );
+	//wp_enqueue_script( 'initiate-axe', plugins_url( '/includes/axe/initiate-axe.js' , __FILE__ ), array( 'axe' ) );
 
 	// Enqueue tota11y - goes in header
-	wp_enqueue_script( 'tota11y', plugins_url( '/includes/tota11y/tota11y.min.js' , __FILE__ ) );
+    if ( current_user_can( 'view_tota11y' ) )
+    	wp_enqueue_script( 'tota11y', plugins_url( '/includes/tota11y/tota11y.min.js' , __FILE__ ) );
 
 }
