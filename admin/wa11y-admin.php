23014ad790cdf1a532e13c96e4c91ba835d4ffa6
Rachel Carden <bamadesigner@gmail.com>
2015-07-09 23:51:21 -0500

    Now including WAVE. Cleaned up the admin.
    Added WAVE as an admin menu bar item. Removed the “Enable Tools” meta
    box. Cleaned up some logic for including tota11y.

--- admin/wa11y-admin.php
+++ admin/wa11y-admin.php
@@ -72,11 +72,44 @@ function wa11y_enqueue_admin_styles( $hook_suffix ) {
 			// Get tota11y settings
 			$wa11y_tota11y_settings = isset( $wa11y_settings[ 'tools' ] ) && isset( $wa11y_settings[ 'tools' ][ 'tota11y' ] ) ? $wa11y_settings[ 'tools' ][ 'tota11y' ] : array();
 
-			// Load tota11y in the admin if set
+			// Should we load tota11y in the admin?
 			if ( isset( $wa11y_tota11y_settings[ 'load_in_admin' ] ) && $wa11y_tota11y_settings[ 'load_in_admin' ] > 0 ) {
 
-				// This file belongs in the header
-				wp_enqueue_script( 'tota11y', plugins_url( '/includes/tota11y/tota11y.min.js', dirname( __FILE__ ) ) );
+				// Will be true by default
+				$load_tota11y = true;
+
+				// If user roles are set, turn off it not a user role
+				if ( isset( $wa11y_tota11y_settings[ 'load_user_roles' ] ) && is_array( $wa11y_tota11y_settings[ 'load_user_roles' ] ) ) {
+
+					// Get current user
+					if ( ( $current_user = wp_get_current_user() )
+						&& ( $current_user_roles = isset( $current_user->roles ) ? $current_user->roles : false )
+						&& is_array( $current_user_roles ) ) {
+
+						// Find out if they share values
+						$user_roles_intersect = array_intersect( $wa11y_tota11y_settings[ 'load_user_roles' ], $current_user_roles );
+
+						// If they do not intersect, turn off
+						if ( empty( $user_roles_intersect ) ) {
+							$load_tota11y = false;
+						}
+
+					}
+
+				}
+
+				// If user capability is set, turn off if not capable
+				if ( ! empty( $wa11y_tota11y_settings[ 'load_user_capability' ] ) ) {
+					$load_tota11y = current_user_can( $wa11y_tota11y_settings[ 'load_user_capability' ] );
+				}
+
+				// We need to load tota11y
+				if ( $load_tota11y ) {
+
+					// This file belongs in the header
+					wp_enqueue_script( 'tota11y', plugins_url( '/includes/tota11y/tota11y.min.js', dirname( __FILE__ ) ) );
+
+				}
 
 			}
 
@@ -130,11 +163,11 @@ function wa11y_add_options_meta_boxes() {
 	// Save Changes
 	add_meta_box( 'wa11y-save-changes', __( 'Save Changes', 'wa11y' ), 'wa11y_print_options_meta_boxes', $wa11y_options_page, 'side', 'core', $wa11y_settings );
 
-	// Enable Tools
-	add_meta_box( 'wa11y-enable-tools', __( 'Enable Tools', 'wa11y' ), 'wa11y_print_options_meta_boxes', $wa11y_options_page, 'normal', 'core', $wa11y_settings );
-
 	// tota11y Settings
-	add_meta_box( 'wa11y-tota11y-settings', __( 'tota11y Settings', 'wa11y' ), 'wa11y_print_options_meta_boxes', $wa11y_options_page, 'normal', 'core', $wa11y_settings );
+	add_meta_box( 'wa11y-tota11y-settings', 'tota11y', 'wa11y_print_options_meta_boxes', $wa11y_options_page, 'normal', 'core', $wa11y_settings );
+
+	// WAVE Settings
+	add_meta_box( 'wa11y-wave-settings', sprintf( __( '%1$s (Web Accessibility Evaluation Tool)', 'wa11y' ), 'WAVE' ), 'wa11y_print_options_meta_boxes', $wa11y_options_page, 'normal', 'core', $wa11y_settings );
 
 }
 
@@ -149,6 +182,9 @@ function wa11y_print_options_meta_boxes( $post, $metabox ) {
 
 	// Get the saved settings passed to the meta boxes
 	$wa11y_settings = isset( $metabox[ 'args' ] ) ? $metabox[ 'args' ] : array();
+
+	// Get enable tools settings
+	$wa11y_enable_tools_settings = isset( $wa11y_settings[ 'enable_tools' ] ) ? $wa11y_settings[ 'enable_tools' ] : array();
 
 	switch( $metabox[ 'id' ] ) {
 
@@ -169,24 +205,6 @@ function wa11y_print_options_meta_boxes( $post, $metabox ) {
 			echo submit_button( __( 'Save Your Changes', 'wa11y' ), 'primary', 'save_wa11y_settings', false, array( 'id' => 'save-wa11y-settings-mb' ) );
 			break;
 
-		// Enable Tools
-		case 'wa11y-enable-tools':
-
-			// Get enable tools settings
-			$wa11y_enable_tools_settings = isset( $wa11y_settings[ 'enable_tools' ] ) ? $wa11y_settings[ 'enable_tools' ] : array();
-
-			?><fieldset>
-				<ul id="wa11y-enable-tools-list">
-					<li>
-						<input class="tool-checkbox" id="tota11y" type="checkbox" name="wa11y_settings[enable_tools][]" value="tota11y"<?php checked( is_array( $wa11y_enable_tools_settings ) && in_array( 'tota11y', $wa11y_enable_tools_settings) ); ?> />
-						<label class="tool-label" for="tota11y">tota11y</label> <a class="view-tool-settings" href="#wa11y-tota11y-settings">(view settings)</a>
-						<p class="tool-desc"><?php printf( __( '%1$s%2$s%3$s is a single JavaScript file that inserts a small button in the bottom corner of your document and helps visualize how your site performs with assistive technologies.', 'wa11y' ), '<a href="http://khan.github.io/tota11y/" target="_blank">', 'tota11y', '</a>' ); ?></p>
-					</li>
-				</ul>
-			</fieldset><?php
-
-			break;
-
 		// tota11y Settings
 		case 'wa11y-tota11y-settings':
 
@@ -197,32 +215,48 @@ function wa11y_print_options_meta_boxes( $post, $metabox ) {
 			$user_roles = get_editable_roles();
 
 			?><div class="wa11y-tool-settings">
-				<p class="tool-desc"><?php printf( __( '%1$s%2$s%3$s is a single JavaScript file that inserts a small button in the bottom corner of your document and helps visualize how your site performs with assistive technologies.', 'wa11y' ), '<a href="http://khan.github.io/tota11y/" target="_blank">', 'tota11y', '</a>' ); ?></p>
+				<input class="tool-checkbox" id="tota11y" type="checkbox" name="wa11y_settings[enable_tools][]" value="tota11y"<?php checked( is_array( $wa11y_enable_tools_settings ) && in_array( 'tota11y', $wa11y_enable_tools_settings) ); ?> />
+				<label class="tool-label" for="tota11y"><?php printf( __( 'Enable %1$s', 'wa11y' ), 'tota11y' ); ?></label>
+				<p class="tool-desc"><?php printf( __( '%1$s%2$s%3$s is a single JavaScript file that inserts a small button in the bottom corner of your document and helps visualize how your site performs with assistive technologies.', 'wa11y' ), '<a href="http://khan.github.io/tota11y/" target="_blank">', 'tota11y', '</a>' ); ?> <strong><em><?php _e( 'Unless specified below, this tool will only load on the front-end of your site.', 'wa11y' ); ?></em></strong></p>
 				<fieldset>
 					<ul id="wa11y-tota11y-settings-list" class="tool-settings-list"><?php
 
 						if ( ! empty( $user_roles ) ) {
-							?><li><label class="tool-setting-header"><?php printf( __( 'Only load %1$s for specific user roles', 'wa11y' ), 'tota11y' ); ?>:</label> <?php
+							?><li><label class="tool-option-header"><?php printf( __( 'Only load %1$s for specific user roles', 'wa11y' ), 'tota11y' ); ?>:</label> <?php
 
 								foreach( $user_roles as $user_role_key => $user_role ) {
-									?><input class="tool-checkbox" id="tota11y-user-role-<?php echo $user_role_key; ?>" type="checkbox" name="wa11y_settings[tools][tota11y][load_user_roles][]" value="<?php echo $user_role_key; ?>"<?php checked( isset( $wa11y_tota11y_settings[ 'load_user_roles' ] ) && in_array( $user_role_key, $wa11y_tota11y_settings[ 'load_user_roles' ] ) ); ?> />
-									<label class="tool-label" for="tota11y-user-role-<?php echo $user_role_key; ?>"><?php echo $user_role[ 'name' ]; ?></label><?php
+									?><input class="tool-option-checkbox" id="tota11y-user-role-<?php echo $user_role_key; ?>" type="checkbox" name="wa11y_settings[tools][tota11y][load_user_roles][]" value="<?php echo $user_role_key; ?>"<?php checked( isset( $wa11y_tota11y_settings[ 'load_user_roles' ] ) && in_array( $user_role_key, $wa11y_tota11y_settings[ 'load_user_roles' ] ) ); ?> />
+									<label class="tool-option-label" for="tota11y-user-role-<?php echo $user_role_key; ?>"><?php echo $user_role[ 'name' ]; ?></label><?php
 								}
 
 							?></li><?php
 						}
 
-						?><li><label class="tool-setting-header" for="tota11y-user-capability"><?php printf( __( 'Only load %1$s for a specific user capability', 'wa11y' ), 'tota11y' ); ?>:</label> <input id="tota11y-user-capability" type="text" name="wa11y_settings[tools][tota11y][load_user_capability]" value="<?php echo isset( $wa11y_tota11y_settings[ 'load_user_capability' ] ) ? $wa11y_tota11y_settings[ 'load_user_capability' ] : null; ?>" /></li>
+						?><li><label class="tool-option-header" for="tota11y-user-capability"><?php printf( __( 'Only load %1$s for a specific user capability', 'wa11y' ), 'tota11y' ); ?>:</label> <input id="tota11y-user-capability" type="text" name="wa11y_settings[tools][tota11y][load_user_capability]" value="<?php echo isset( $wa11y_tota11y_settings[ 'load_user_capability' ] ) ? $wa11y_tota11y_settings[ 'load_user_capability' ] : null; ?>" /></li>
 
-						<li><label class="tool-setting-header" for="tota11y-admin"><?php printf( __( 'Load %1$s in the admin', 'wa11y' ), 'tota11y' ); ?>:</label>
-							<input class="tool-checkbox" id="tota11y-admin-yes" type="radio" name="wa11y_settings[tools][tota11y][load_in_admin]" value="1"<?php checked( isset( $wa11y_tota11y_settings[ 'load_in_admin' ] ) && $wa11y_tota11y_settings[ 'load_in_admin' ] > 0 ); ?> />
-							<label class="tool-label" for="tota11y-admin-yes"><?php _e( 'Yes', 'wa11y' ); ?></label>
-							<input class="tool-checkbox" id="tota11y-admin-no" type="radio" name="wa11y_settings[tools][tota11y][load_in_admin]" value="0"<?php checked( ! ( isset( $wa11y_tota11y_settings[ 'load_in_admin' ] ) && $wa11y_tota11y_settings[ 'load_in_admin' ] > 0 ) ); ?> />
-							<label class="tool-label" for="tota11y-admin-no"><?php _e( 'No', 'wa11y' ); ?></label>
+						<li><label class="tool-option-header" for="tota11y-admin"><?php printf( __( 'Load %1$s in the admin', 'wa11y' ), 'tota11y' ); ?>:</label>
+							<input class="tool-option-checkbox" id="tota11y-admin-yes" type="radio" name="wa11y_settings[tools][tota11y][load_in_admin]" value="1"<?php checked( isset( $wa11y_tota11y_settings[ 'load_in_admin' ] ) && $wa11y_tota11y_settings[ 'load_in_admin' ] > 0 ); ?> />
+							<label class="tool-option-label" for="tota11y-admin-yes"><?php _e( 'Yes', 'wa11y' ); ?></label>
+							<input class="tool-option-checkbox" id="tota11y-admin-no" type="radio" name="wa11y_settings[tools][tota11y][load_in_admin]" value="0"<?php checked( ! ( isset( $wa11y_tota11y_settings[ 'load_in_admin' ] ) && $wa11y_tota11y_settings[ 'load_in_admin' ] > 0 ) ); ?> />
+							<label class="tool-option-label" for="tota11y-admin-no"><?php _e( 'No', 'wa11y' ); ?></label>
 
 					</ul>
+					<p class="tool-settings-list-desc"><?php printf( __( 'If no user roles are selected or user capability is provided, %1$s will load for all logged-in users.', 'wa11y' ), 'tota11y' ); ?></p>
 				</fieldset>
 			</div> <!-- .wa11y-tool-settings --><?php
+
+			break;
+
+		// WAVE Settings
+		case 'wa11y-wave-settings':
+
+			?><div class="wa11y-tool-settings">
+				<input class="tool-checkbox" id="wave" type="checkbox" name="wa11y_settings[enable_tools][]" value="wave"<?php checked( is_array( $wa11y_enable_tools_settings ) && in_array( 'wave', $wa11y_enable_tools_settings) ); ?> />
+				<label class="tool-label" for="wave"><?php printf( __( 'Enable %1$s (Web Accessibility Evaluation Tool)', 'wa11y' ), 'WAVE' ); ?></label>
+				<p class="tool-desc"><?php printf( __( '%1$s%2$s%3$s is a free evaluation tool provided by WebAIM (Web Accessibility In Mind). It can be used to evaluate a live website for a wide range of accessibility issues.', 'wa11y' ), '<a href="http://webaim.org/" target="_blank">', 'WAVE', '</a>' ); ?></p>
+				<fieldset>
+				</fieldset>
+			</div><?php
 
 			break;
 
--- wa11y.php
+++ wa11y.php
@@ -28,7 +28,14 @@ if( is_admin() ) {
 
 // Get Wa11y settings
 function wa11y_get_settings() {
-	return get_option( 'wa11y_settings', array(
+	global $wa11y_settings;
+
+	// If we've already retrieved them then we're good to go
+	if ( isset( $wa11y_settings ) && ! empty( $wa11y_settings ) ) {
+		return $wa11y_settings;
+	}
+
+	return $wa11y_settings = get_option( 'wa11y_settings', array(
 		'tools' => array(
 			'tota11y' => array(
 				'load_user_roles'       => array( 'administrator' ),
@@ -37,6 +44,68 @@ function wa11y_get_settings() {
 			)
 		)
 	) );
+}
+
+/**
+ * Add items to the admin bar.
+ *
+ * @since   1.0
+ * @param 	WP_Admin_Bar - $wp_admin_bar - WP_Admin_Bar instance, passed by reference
+ */
+add_action( 'admin_bar_menu', 'wa11y_add_to_admin_bar', 100 );
+function wa11y_add_to_admin_bar( $wp_admin_bar ) {
+
+	// Right now it's only on the front end
+	if ( is_admin() ) {
+		return;
+	}
+
+	// Get our settings
+	$wa11y_settings = wa11y_get_settings();
+
+	// Only need to worry about this stuff if we have enabled tools
+	$wa11y_enable_tools = isset( $wa11y_settings[ 'enable_tools' ] ) ? $wa11y_settings[ 'enable_tools' ] : array();
+	if ( empty( $wa11y_enable_tools ) )
+		return;
+
+	// Will hold all of the Wa11y nodes
+	$wa11y_nodes = array();
+
+	// If WAVE is enabled...
+    if ( in_array( 'wave', $wa11y_enable_tools ) ) {
+
+	    // Get wave settings
+	    //$wa11y_wave_settings = isset( $wa11y_settings[ 'tools' ] ) && isset( $wa11y_settings[ 'tools' ][ 'wave' ] ) ? $wa11y_settings[ 'tools' ][ 'wave' ] : array();
+
+		// Build the current URL
+		$current_url = ( ! ( isset( $_SERVER[ 'HTTPS' ] ) && $_SERVER[ 'HTTPS' ] == 'on' ) ? 'http://' : 'https://' ) . $_SERVER[ 'SERVER_NAME' ] . ( isset( $_SERVER[ 'REQUEST_URI' ] ) ? $_SERVER[ 'REQUEST_URI' ] : NULL );
+
+		// Add WAVE node
+		$wa11y_nodes[] = array(
+			'id'    	=> 'wa11y-wave',
+			'title' 	=> 'WAVE Evaluation',
+			'href'		=> 'http://wave.webaim.org/report#/' . urlencode( $current_url ),
+			'meta'		=> array( 'target' => '_blank' ),
+			);
+
+	}
+
+	if ( ! empty( $wa11y_nodes ) ) {
+
+		// Add parent Wa11y node
+		$wp_admin_bar->add_node( array(
+			'id'    	=> 'wa11y',
+			'title' 	=> 'Wa11y',
+			'parent'	=> false,
+		));
+
+		// Add child nodes
+		foreach( $wa11y_nodes as $node ) {
+			$wp_admin_bar->add_node( array_merge( $node, array( 'parent' => 'wa11y' ) ) );
+		}
+
+	}
+
 }
 
 // Load the script tools
@@ -57,10 +126,10 @@ function wa11y_load_script_tools() {
 	    // Get tota11y settings
 	    $wa11y_tota11y_settings = isset( $wa11y_settings[ 'tools' ] ) && isset( $wa11y_settings[ 'tools' ][ 'tota11y' ] ) ? $wa11y_settings[ 'tools' ][ 'tota11y' ] : array();
 
-	    // Will be true if we should load tota11y
-	    $load_tota11y = false;
+	    // Will be true if we should load tota11y - by default, load if logged in
+	    $load_tota11y = is_user_logged_in();
 
-	    // Load by user role
+	    // If user roles are set, turn off it not a user role
 	    if ( isset( $wa11y_tota11y_settings[ 'load_user_roles' ] ) && is_array( $wa11y_tota11y_settings[ 'load_user_roles' ] ) ) {
 
 		    // Get current user
@@ -71,18 +140,18 @@ function wa11y_load_script_tools() {
 			    // Find out if they share values
 			    $user_roles_intersect = array_intersect( $wa11y_tota11y_settings[ 'load_user_roles' ], $current_user_roles );
 
-			    // If they intersect at all...
-			    if ( ! empty( $user_roles_intersect ) ) {
-				    $load_tota11y = true;
+			    // If they do not intersect, turn off
+			    if ( empty( $user_roles_intersect ) ) {
+				    $load_tota11y = false;
 				}
 
 		    }
 
 	    }
 
-	    // Load by user capability
-	    if ( ! $load_tota11y && isset( $wa11y_tota11y_settings[ 'load_user_capability' ] ) && current_user_can( $wa11y_tota11y_settings[ 'load_user_capability' ] ) ) {
-		    $load_tota11y = true;
+	    // If user capability is set, turn off if not capable
+	    if ( ! empty( $wa11y_tota11y_settings[ 'load_user_capability' ] ) ) {
+		    $load_tota11y = current_user_can( $wa11y_tota11y_settings[ 'load_user_capability' ] );
 	    }
 
 	    // We need to load tota11y
